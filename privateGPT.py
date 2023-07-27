e9763c3ee70fd37ff17e8715be4ad67fe08b25c7
Nicholas Davidson <59320455+ndavidson19@users.noreply.github.com>
2023-07-26 17:20:18 -0700

    added gui to privateGPT

--- privateGPT.py
+++ privateGPT.py
@@ -5,6 +5,8 @@ from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.vectorstores import Chroma
 from langchain.llms import GPT4All, LlamaCpp
+import tkinter as tk
+from tkinter import ttk, scrolledtext
 import os
 import argparse
 import time
@@ -22,12 +24,76 @@ target_source_chunks = int(os.environ.get('TARGET_SOURCE_CHUNKS',4))
 
 from constants import CHROMA_SETTINGS
 
+embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
+db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
+retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+
+def send(*args):
+    query = entry_field.get()
+    chat_history.insert(tk.END, f"You: {query}\n")
+    # Get the answer from the chain
+    start = time.time()
+    res = qa(query)
+    answer, docs = res['result'], []
+    end = time.time()
+
+    # Append the result to chat history
+    chat_history.insert(tk.END, f"\n\n> Answer (took {round(end - start, 2)} s.):\n{answer}\n")
+
+    # Append the relevant sources used for the answer
+    for document in docs:
+        chat_history.insert(tk.END, f"\n> {document.metadata['source']}:\n{document.page_content}\n")
+
+    entry_field.delete(0, tk.END)
+
+root = tk.Tk()
+root.title("Chatbot")
+root.geometry('1200x800')  # Set the size of the window
+
+frame = tk.Frame(root, bg='light gray')
+frame.grid(sticky='nsew')
+
+root.grid_rowconfigure(0, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+chat_history = scrolledtext.ScrolledText(frame, bg='light gray')
+chat_history.grid(row=0, column=0, sticky='nsew')
+chat_history.tag_config('user', foreground='blue')
+chat_history.tag_config('bot', foreground='green')
+
+entry_field = tk.Entry(frame, font=("Arial", 14))
+entry_field.grid(row=1, column=0, sticky='nsew')
+entry_field.bind("<Return>", send)
+
+send_button = tk.Button(frame, text="Send", command=send)
+send_button.grid(row=2, column=0)
+
+
+callbacks = [StreamingStdOutCallbackHandler()]
+
+# Prepare the LLM
+match model_type:
+    case "LlamaCpp":
+        llm = LlamaCpp(model_path=model_path, max_tokens=model_n_ctx, n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+    case "GPT4All":
+        llm = GPT4All(model=model_path, max_tokens=model_n_ctx, backend='gptj', n_batch=model_n_batch, callbacks=callbacks, verbose=False)
+    case _default:
+        # raise exception if model_type is not supported
+        raise Exception(f"Model type {model_type} is not supported. Please choose one of the following: LlamaCpp, GPT4All")
+    
+qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+root.mainloop()
+
+
 def main():
+
     # Parse the command line arguments
     args = parse_arguments()
     embeddings = HuggingFaceEmbeddings(model_name=embeddings_model_name)
     db = Chroma(persist_directory=persist_directory, embedding_function=embeddings, client_settings=CHROMA_SETTINGS)
     retriever = db.as_retriever(search_kwargs={"k": target_source_chunks})
+
     # activate/deactivate the streaming StdOut callback for LLMs
     callbacks = [] if args.mute_stream else [StreamingStdOutCallbackHandler()]
     # Prepare the LLM
@@ -42,6 +108,7 @@ def main():
         
     qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents= not args.hide_source)
     # Interactive questions and answers
+    root.mainloop()
     while True:
         query = input("\nEnter a query: ")
         if query == "exit":
